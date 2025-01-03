290626807e0869621373426bff53aedc7fd08313
Megh2005 <megh.deb.civil27@heritageit.edu.in>
2025-01-03 16:52:43 +0530

    add contact section with form and footer; implement GitHub and LinkedIn links in script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("github").addEventListener("click", function () {
+    window.open("https://github.com/Megh2005", "_blank");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("linkedin").addEventListener("click", function () {
+    window.open("https://www.linkedin.com/in/megh-deb", "_blank");
+  });
+});
